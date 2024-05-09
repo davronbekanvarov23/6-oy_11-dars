dfb13e0d72de0fce4006472baebb1660a1ae176d
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-05-09 07:40:55 +0500

    changed

--- src/components/BooksContent.jsx
+++ src/components/BooksContent.jsx
@@ -6,19 +6,17 @@ import { TbLayoutList, TbLayoutGrid } from "react-icons/tb";
 
 function BooksContent() {
   const [layoutState, setLayoutState] = useState("grid");
-  const activeStyle = (layout) => {
-    return layoutState == layout ? "btn btn-circle bg-red" : "btn btn-circle bg-blue";
-  };
+  const activeStyle = "btn btn-circle ";
 
   return (
     <>
       <div className="mb-5 border-b py-3 flex justify-between items-center">
         <h3 className="font-medium text-2xl">10 books</h3>
         <div className="flex gap-3">
-          <button className="activeStyle grid">
+          <button className={activeStyle} id="grid">
             <TbLayoutGrid className="w-7 h-7" />
           </button>
-          <button className="activeStyle list">
+          <button className={activeStyle} id="list">
             <TbLayoutList className="w-7 h-7" />
           </button>
         </div>
