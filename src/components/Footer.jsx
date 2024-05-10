a8a0c14d8ee27ef0e17e80a7fb74ce870a213e60
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-05-11 00:03:57 +0500

    changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
       <aside>
-        <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        <p>Copyright © {year} - All right reserved by ACME Industries Ltd</p>
       </aside>
     </footer>
   );
